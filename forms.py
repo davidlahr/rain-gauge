fcc4817c1d80a01d302c062d556dd887f9a0da50
davidlahr <85328443+davidlahr@users.noreply.github.com>
2022-06-25 10:03:43 -0400

    default values

--- forms.py
+++ forms.py
@@ -3,12 +3,13 @@ from wtforms.fields import *
 from wtforms import StringField, SubmitField, IntegerField, FloatField, DateField, RadioField, FieldList, FormField
 from wtforms.validators import DataRequired, InputRequired, Optional
 from flask_wtf import FlaskForm, Form
-# from wtforms.fields.html5 import DateField
+# from wtforms.fields.html5 import DateFi
+from datetime import date
 
 
 class CheckRain(FlaskForm):
-    address = StringField('Enter a City and State OR Zip Code', validators=[DataRequired()])
-    start_date = DateField('Date From', [Optional()])
-    end_date = DateField('Date to', [Optional()])
+    address = StringField('Enter a City and State OR Zip Code',default='Chicago, IL', validators=[DataRequired()])
+    start_date = DateField('Date From', default=date.today())
+    end_date = DateField('Date to', default=date.today())
 
     submit = SubmitField("Get Rainfall Total")
